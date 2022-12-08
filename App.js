467683b0ae0f4e4279a99140b8336ea1fd9d4e7e
Gabriel Severino <gabrielseverinooo000@gmail.com>
2022-12-08 00:04:12 -0300

    tela login

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Button, TextInput} from 'react-native';
+import { Avatar } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input} from 'react-native-elements';
+import { Text } from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<View style={styles.container}>
+    <View style={{alignItems: 'center'}}>
+
+       <Avatar
+
+            size="large"rounded source={{ uri:'https://icones.pro/wp-content/uploads/2021/02/icone-utilisateur-gris.png',}}/>
+
+    </View>  
+    <View style={{marginTop:30, width:200,}}>
+
+
+      <Text>Login:</Text>
+        <TextInput style={{backgroundColor: 'white', height:30}}/>
+          
+      <Text>Senha:</Text>
+        <TextInput style={{backgroundColor: 'white', height:30}}/>
+      
+
+   </View>
+      <View style={{marginTop:30, width:200}}>
+            <Button title="Login"/>
+       
+      </View>
+   <View style={{marginTop:20, width:200}}>
+           <Button title="Cadastre-se"
+                     color={'red'}/>
+
+   </View>
+</View>
   );
 }
 
@@ -16,5 +46,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f3dbc3',
   },
 });
